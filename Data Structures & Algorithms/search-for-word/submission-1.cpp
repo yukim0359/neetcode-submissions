b463792fd56372647c22df013fb0dbdbceeaf9b7
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-14 17:57:47 +0900

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int n, m;
+
+    bool dfs(int i, int j, vector<vector<char>>& board, string word, int pos) {
+        if (board[i][j] != word[pos]) return false;
+        if (pos == word.size() - 1) return true;
+        if (i != 0 && dfs(i - 1, j, board, word, pos + 1)) {
+            return true;
+        }
+        if (i != n - 1 && dfs(i + 1, j, board, word, pos + 1)) {
+            return true;
+        }
+        if (j != 0 && dfs(i, j - 1, board, word, pos + 1)) {
+            return true;
+        }
+        if (j != m - 1 && dfs(i, j + 1, board, word, pos + 1)) {
+            return true;
+        }
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        n = board.size();
+        m = board[0].size();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (dfs(i, j, board, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+};
