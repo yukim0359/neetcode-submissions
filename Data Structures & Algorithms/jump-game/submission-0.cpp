5a366f9d01e7dd00a0e468cc8ad9191757e82277
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-15 13:20:31 +0900

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = (int)nums.size();
+        vector<bool> dp(n, false);
+        dp[0] = true;
+
+        // int right = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (!dp[i]) continue;
+            for (int j = 0; j <= nums[i]; ++j) {
+                if (i + j < n) dp[i + j] = true;
+            }
+        }
+        return dp[n - 1];
+    }
+};
