e81d75a2958123bba0b1a369b2cf11d232a0ec15
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-15 13:54:57 +0900

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    Node* dfs(Node* node) {
+        if (mp.count(node)) return mp[node];
+
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
+        for (Node* nei : node->neighbors) {
+            clone->neighbors.push_back(dfs(nei));
+        }
+        return clone;
+    }
+    
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr) return nullptr;
+        return dfs(node);
+    }
+};
