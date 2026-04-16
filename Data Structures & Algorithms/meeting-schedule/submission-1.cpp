8f1234b9dbe84815f2e54037a5642bc915e3e144
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-16 23:47:15 +0900

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.size() <= 1) return true;
+        sort(intervals.begin(), intervals.end(),
+            [](const Interval& a, const Interval& b) {
+                return a.start < b.start;
+            }
+        );
+        for (int i = 0; i < intervals.size() - 1; ++i) {
+            if (intervals[i].end > intervals[i + 1].start) return false;
+        }
+        return true;
+    }
+};
