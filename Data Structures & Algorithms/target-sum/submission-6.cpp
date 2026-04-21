4db2ef413481748d0edd229b9844b0bdb2c55331
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-21 13:31:46 +0900

    Add: target-sum - submission-6

---
+++ Data Structures & Algorithms/target-sum/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int sum = 0;
+        for (int x : nums) sum += x;
+        vector<unordered_map<int, int>> dp(n + 1);
+        dp[0][0] = 1;
+
+        for (int i = 0; i < n; ++i) {
+            for (auto& [s, cnt] : dp[i]) {
+                dp[i + 1][s + nums[i]] += cnt;
+                dp[i + 1][s - nums[i]] += cnt;
+            }
+        }
+
+        return dp[n][target];
+    }
+};
