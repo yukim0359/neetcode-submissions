f0b0dc523cf2da5f7921d60b8c3ffc9695fc1d4c
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-22 15:13:24 +0900

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,27 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> mp;
+
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+        return;
+    }
+    
+    string get(string key, int timestamp) {
+        vector<pair<int, string>> v = mp[key];
+        int n = v.size();
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (v[mid].first == timestamp) return v[mid].second;
+            else if (v[mid].first < timestamp) l = mid + 1;
+            else if (v[mid].first > timestamp) r = mid - 1;
+        }
+
+        if (l > r) return v[r].second;
+        else if (l < r) return (l == -1) ? "" : v[l].second;
+    }
+};
