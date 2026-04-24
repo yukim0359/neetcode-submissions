a645d74b0b0d8147d2b7de00476436f75a0399c7
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-24 14:58:14 +0900

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return 0;
+
+        int count = 1;
+        int l = 0, r = 0;
+        while (true) {
+            int farthest = l;
+            for (int i = l; i <= r; ++i) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            if (farthest >= n - 1) return count;
+            count++;
+            l = r + 1, r = farthest;
+        }
+    }
+};
