94f2dee4c005d3a0291c410ef40ef3457510a50d
Yuki Maeda <147034352+yukim0359@users.noreply.github.com>
2026-04-24 18:14:45 +0900

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> path;
+
+    bool dfs(int course, vector<vector<int>>& graph, vector<int>& state) {
+        if (state[course] == 1) return false;
+        if (state[course] == 2) return true;
+
+        state[course] = 1;
+        path.push_back(course);
+
+        for (int next : graph[course]) {
+            if (!dfs(next, graph, state)) return false;
+        }
+
+        state[course] = 2;
+        return true;
+    }
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> inDeg(numCourses, 0);
+        for (auto& v : prerequisites) {
+            int e = v[0], s = v[1];
+            graph[e].push_back(s);
+            inDeg[s]++;
+        }
+
+        vector<int> state(numCourses, 0);
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDeg[i] == 0) {
+                if (!dfs(i, graph, state)) return {};
+                reverse(path.begin(), path.end());
+                ans.insert(ans.end(), path.begin(), path.end());
+                path = {};
+            }
+        }
+
+        return ans;
+    }
+};
